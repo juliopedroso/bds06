82251d41c1b677dcfc657720ac2fb6e4171976e8
Julio Cesar Pedroso <juliocp@weg.net>
2022-06-28 12:40:26 -0300

    add validation

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.services.MovieService;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -19,12 +22,19 @@ public class MovieController {
     @Autowired
     private MovieService service;
 
-
     @GetMapping
     public ResponseEntity<List<MovieDTO>> findAll() {
         List<MovieDTO> list =  service.findAll();
 
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
+
+        MovieDTO movie = service.findById(id);
+
+        return ResponseEntity.ok(movie);
     }
     
     
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.movieflix.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class MovieService {
@@ -22,4 +24,10 @@ public class MovieService {
         return movies.stream().map(MovieDTO::new).collect(Collectors.toList());
     }
 
+    public MovieDTO findById(Long id) {
+        Optional<Movie> obj = repository.findById(id);
+        Movie movie = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
+        return new MovieDTO(movie);
+    }
 }
